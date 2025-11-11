7aee0261e4d84df852ace730113dc39bc480118b
rremling <rremling@gmail.com>
2025-11-11 12:10:38 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
     icon: "/favicon.ico?v=5",
     apple: "/apple-touch-icon.png?v=5",
   },
-  manifest: "/manifest.json", 
+  manifest: "/manifest.webmanifest", 
 };
 
 export default function RootLayout({
